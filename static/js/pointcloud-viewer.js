4a857237262965b3c417108ebe98318f3d4c6347
XYunaaa <18640133823@163.com>
2025-08-07 14:26:39 +0800

    test function

--- static/js/pointcloud-viewer.js
+++ static/js/pointcloud-viewer.js
@@ -10,13 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
     renderer.setSize(width, height);
     viewer.appendChild(renderer.domElement);
   
-    const loader = new THREE.PLYLoader();
-    loader.load('assets/videos/0.ply', function (geometry) {
-        const material = new THREE.PointsMaterial({ size: 0.2, vertexColors: true });
-        const points = new THREE.Points(geometry, material);
-        scene.add(points);
-    });
-  
+    // const loader = new THREE.PLYLoader();
+    // loader.load('assets/videos/0.ply', function (geometry) {
+    //     const material = new THREE.PointsMaterial({ size: 0.2, vertexColors: true });
+    //     const points = new THREE.Points(geometry, material);
+    //     scene.add(points);
+    // });
+    const positions = [];
+    for (let i = 0; i < 1000; i++) {
+        positions.push(
+        THREE.MathUtils.randFloatSpread(10), // x: -5~5
+        THREE.MathUtils.randFloatSpread(10),
+        THREE.MathUtils.randFloatSpread(10)
+        );
+    }
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    const material = new THREE.PointsMaterial({ color: 0xff3366, size: 0.2 }); // size不能太小
+    const pointCloud = new THREE.Points(geometry, material);
+    scene.add(pointCloud);
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
   
     function animate() {
