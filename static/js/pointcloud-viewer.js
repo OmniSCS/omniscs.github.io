c343966370d9af83d58ff8776012162ae034aab2
XYunaaa <18640133823@163.com>
2025-08-07 15:11:42 +0800

    test function

--- static/js/pointcloud-viewer.js
+++ static/js/pointcloud-viewer.js
@@ -9,28 +9,29 @@ document.addEventListener('DOMContentLoaded', function() {
     camera.position.z = 5;
   
     const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff, 1);
     renderer.setSize(width, height);
     viewer.appendChild(renderer.domElement);
   
-    // const loader = new THREE.PLYLoader();
-    // loader.load('assets/videos/0.ply', function (geometry) {
-    //     const material = new THREE.PointsMaterial({ size: 0.2, vertexColors: true });
-    //     const points = new THREE.Points(geometry, material);
-    //     scene.add(points);
-    // });
-    const positions = [];
-    for (let i = 0; i < 1000; i++) {
-        positions.push(
-        THREE.MathUtils.randFloatSpread(10), // x: -5~5
-        THREE.MathUtils.randFloatSpread(10),
-        THREE.MathUtils.randFloatSpread(10)
-        );
-    }
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-    const material = new THREE.PointsMaterial({ color: 0xff3366, size: 0.2 }); // size不能太小
-    const pointCloud = new THREE.Points(geometry, material);
-    scene.add(pointCloud);
+    const loader = new THREE.PLYLoader();
+    loader.load('assets/videos/0.ply', function (geometry) {
+        const material = new THREE.PointsMaterial({ size: 0.2, vertexColors: true });
+        const points = new THREE.Points(geometry, material);
+        scene.add(points);
+    });
+    // const positions = [];
+    // for (let i = 0; i < 1000; i++) {
+    //     positions.push(
+    //     THREE.MathUtils.randFloatSpread(10), // x: -5~5
+    //     THREE.MathUtils.randFloatSpread(10),
+    //     THREE.MathUtils.randFloatSpread(10)
+    //     );
+    // }
+    // const geometry = new THREE.BufferGeometry();
+    // geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    // const material = new THREE.PointsMaterial({ color: 0xff3366, size: 0.2 }); // size不能太小
+    // const pointCloud = new THREE.Points(geometry, material);
+    // scene.add(pointCloud);
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
   
     function animate() {
